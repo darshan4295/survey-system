a8463ae50b744980d1e99e8bd5f2d74f1e539675
DARSHAN HANDE <dhande43@gmail.com>
2025-01-07 10:55:29 +0530

    dasddads

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
- 
+
+// The simplest way to make webhook routes public
 export default clerkMiddleware();
- 
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Exclude files with extension and next internal routes
+    "/((?!.*\\..*|_next).*)",
+    // Include root route
+    "/",
+    // Include api routes except webhooks
+    "/(api(?!/webhooks).*)"
+  ]
 };
